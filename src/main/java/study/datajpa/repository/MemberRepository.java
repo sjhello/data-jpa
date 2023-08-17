ed41ec0d7fc3baa3c6b420d5df483981aa621d22
sjhello <sjhello235@gmail.com>
2023-08-17 16:55:16 +0900

    파라미터 바인딩, 컬렉션 파라미터 바인딩, 단순 값 조회, DTO로 직접 조회

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -16,4 +17,16 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") Integer age);
+
+    @Query("select m from Member m where m.username = :username")
+    List<Member> findUsers(@Param("username") String username);
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernames();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 
@@ -19,6 +21,9 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     @DisplayName("이름과_나이로_회원을_조회한다")
@@ -59,5 +64,57 @@ class MemberRepositoryTest {
 
         List<Member> members = memberRepository.findUser("sjhello", 30);
         assertThat(members).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("이름으로 회원을 조회한다 - 파라미터 바인딩")
+    void 회원을_조회한다() {
+        Member sjhello = new Member("sjhello", 30);
+        Member bailey = new Member("sjhello", 20);
+
+        memberRepository.save(sjhello);
+        memberRepository.save(bailey);
+
+        List<Member> members = memberRepository.findUsers("sjhello");
+        assertThat(members).contains(sjhello, bailey);
+    }
+
+    @Test
+    @DisplayName("회원이름으로 검색한다 - 컬렉션 파라미터 바인딩")
+    void 회원이름으로_검색한다() {
+        Member sjhello = new Member("sjhello");
+        Member bailey = new Member("bailey");
+        memberRepository.save(sjhello);
+        memberRepository.save(bailey);
+
+        List<Member> members = memberRepository.findByNames(List.of("sjhello", "bailey"));
+
+        assertThat(members).contains(sjhello, bailey);
+    }
+
+    @Test
+    @DisplayName("회원이름을_검색한다 - 단순 값 조회")
+    void 회원이름을_검색한다() {
+        Member sjhello = new Member("sjhello");
+        Member bailey = new Member("bailey");
+
+        memberRepository.save(sjhello);
+        memberRepository.save(bailey);
+
+        List<String> usernames = memberRepository.findUsernames();
+        assertThat(usernames).contains("sjhello", "bailey");
+    }
+
+    @Test
+    @DisplayName("회원의_팀을_검색한다 - DTO로 직접 조회")
+    void 회원의_팀을_검색한다() {
+        Team team = new Team("teamA");
+        Member sjhello = new Member("sjhello", 30, team);
+        teamRepository.save(team);
+        memberRepository.save(sjhello);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+
+        assertThat(memberDto).hasSize(1);
     }
 }
